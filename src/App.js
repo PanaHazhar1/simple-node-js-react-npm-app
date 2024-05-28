450d5cc7ed2a7d572d5dc7290cf91e362b0f8cdc
PanaHazhar1 <phqiu190115@uniq.edu.iq>
2024-05-28 15:25:02 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,6 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      items: ['Item 1', 'Item 2', 'Item 3'],
+      newItem: ''
+    };
+  }
+
+  handleChange = (event) => {
+    this.setState({ newItem: event.target.value });
+  };
+
+  addItem = () => {
+    this.setState(state => ({
+      items: [...state.items, state.newItem],
+      newItem: ''
+    }));
+  };
+
   render() {
     return (
       <div className="App">
@@ -13,6 +32,21 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className="App-content">
+          <h2>Item List</h2>
+          <ul>
+            {this.state.items.map((item, index) => (
+              <li key={index}>{item}</li>
+            ))}
+          </ul>
+          <input
+            type="text"
+            value={this.state.newItem}
+            onChange={this.handleChange}
+            placeholder="Add a new item"
+          />
+          <button onClick={this.addItem}>Add Item</button>
+        </div>
       </div>
     );
   }
